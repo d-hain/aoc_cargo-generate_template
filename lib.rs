c6138a3df0005d540d5d245701688eb7f3b4c497
David Hain <57726396+d-hain@users.noreply.github.com>
2022-12-07 01:18:46 +0100

    Added `lib.rs`, containing part1 & 2 functions and tests

---
+++ lib.rs
@@ -0,0 +1,30 @@
+pub fn part1(input: &str) -> u32 {
+    //TODO: Part 1
+
+    0
+}
+
+pub fn part2(input: &str) -> u32 {
+    //TODO: Part 2
+    
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn part1_working(){
+        let example_input = ""; //TODO
+        let example_result = 0; //TODO
+
+        assert_eq!(crate::part1(example_input), example_result);
+    }
+
+    #[test]
+    fn part2_working(){
+        let example_input = ""; //TODO
+        let example_result = 0; //TODO
+
+        assert_eq!(crate::part2(example_input), example_result);
+    }
+}
